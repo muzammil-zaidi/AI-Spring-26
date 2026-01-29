08680809d806b9c6b30f5f3d6fdcf043e8e03b20
Muzammil Zaidi <muzammil.s3517@gmail.com>
2026-01-29 20:49:17 +0500

    Update Q2.py

--- LAB 1/Q2.py
+++ LAB 1/Q2.py
@@ -1,1 +1,22 @@
+numbers = []
 
+print("Enter 10 integers:")
+for i in range(10):
+    num = int(input("Enter number: "))
+    numbers.append(num)
+
+print("\nPrime number check:")
+for num in numbers:
+    if num <= 1:
+        print(num, "is NOT a Prime number")
+    else:
+        prime = True
+        for i in range(2, num):
+            if num % i == 0:
+                prime = False
+                break
+
+        if prime:
+            print(num, "is a Prime number")
+        else:
+            print(num, "is NOT a Prime number")
