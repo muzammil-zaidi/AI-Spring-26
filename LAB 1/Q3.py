6546b7073a751cc70c6e2990bdbf0097f5969031
Muzammil Zaidi <muzammil.s3517@gmail.com>
2026-02-01 03:00:01 +0500

    Update Q3.py
    Check if two input strings are anagrams and replace common characters with '#' for clarity.

--- LAB 1/Q3.py
+++ LAB 1/Q3.py
@@ -1,1 +1,25 @@
+word1=input("Enter the string1: ")
+word2=input("Enter the string2: ")
 
+
+for i in word1:
+    for j in word2:
+        if i==j:
+            word1=word1.replace(i,'#')
+            word2=word2.replace(j,'#')
+            break
+
+
+found=True
+for i in word1:
+        for j in word2:
+            if i!=j:
+
+                print(f"{word1}{word2} is not anagram ")
+                found=False
+                break
+        if found==False:
+         break
+
+if found==True:
+     print("It is anagram")
